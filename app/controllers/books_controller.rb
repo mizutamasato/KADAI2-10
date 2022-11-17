5d35b9c8d3b1c2d8242c6a3e4a4e74dc9ab6a0e3
mizutamasato <117154853+mizutamasato@users.noreply.github.com>
2022-11-17 13:48:28 +0000

    [tochuu]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,22 +4,47 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+    @book.save
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to'top'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "投稿がsuccessfullyしました"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      #renderはアクションを呼び出さずにviewファイルを呼び出す
+      render :index
+    end
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "更新がsuccessfullyしました"
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "破壊がsuccessfullyしました"
+    redirect_to book
+  end
+
   private
-  def list_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 
